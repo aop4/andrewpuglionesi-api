f5e4dd065420c5e5023c2e2ac36fcca2b7e32123
Andrew Puglionesi <aop4@pitt.edu>
2019-01-03 20:37:25 -0500

    Send warning email upon all admin log-ins

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,4 +1,5 @@
 include ActionController::HttpAuthentication::Basic::ControllerMethods
+include EmailHelper
 
 RailsAdmin.config do |config|
 
@@ -41,10 +42,19 @@ RailsAdmin.config do |config|
     # history_show
   end
   
+  # challenge the admin, and in the event of success, send a warning email in
+  # case of a security breech. The email is only sent on success because of
+  # short circuiting (when it's your own solution that's hacky, it can be
+  # oddly appealing).
   config.authenticate_with do
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV.fetch('ADMIN_USERNAME') &&
-      password == ENV.fetch('ADMIN_PASS')
+      password == ENV.fetch('ADMIN_PASS') &&
+      send_email("andrewpuglionesi.com admin interface logged into", 
+      "The admin interface was just logged into. If this wasn't you, "\
+      "immediately change the username and password, consider upping security "\
+      "measures, and check every single thing in the database.")
     end
   end
+  
 end
