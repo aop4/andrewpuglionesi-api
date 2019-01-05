8273659ef844ee55690067e32855b6af967e7548
Andrew Puglionesi <aop4@pitt.edu>
2019-01-04 20:13:50 -0500

    Order news posts by *descending* date

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -1,7 +1,7 @@
 class NewsItemsController < ApplicationController
   
   def index
-    @news_items = NewsItem.order(:item_date)
+    @news_items = NewsItem.order(item_date: :desc)
     render json: @news_items
   end
 
