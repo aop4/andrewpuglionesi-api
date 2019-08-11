9edf7d2f2b6a16e67acb6d4c8250be24259103a3
Andrew Puglionesi <aop4@pitt.edu>
2019-08-11 08:35:33 -0400

    Paginate news items

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -1,8 +1,23 @@
+require_relative '../services/pagination_service'
+
 class NewsItemsController < ApplicationController
-  
+
+  ITEMS_PER_PAGE = 4
+
   def index
-    @news_items = NewsItem.order(item_date: :desc)
-    render json: @news_items
+    requested_page = params[:pageNum].to_i
+    @news_items = retrieve_data(requested_page)
+    render json: { 
+      'data' => @news_items,
+      'numPages' => @pagination_service.get_num_pages,
+      'currPage' => @pagination_service.get_validated_page_num(requested_page)
+    }
+  end
+
+  def retrieve_data(requested_page)
+    @pagination_service = PaginationService.new(ITEMS_PER_PAGE, NewsItem)
+    result_set_offset = @pagination_service.get_result_set_offset(requested_page)
+    NewsItem.order(item_date: :desc).limit(ITEMS_PER_PAGE).offset(result_set_offset)
   end
 
 end
