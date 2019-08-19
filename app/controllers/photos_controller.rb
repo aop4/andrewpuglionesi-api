401a2fc514d8f12348667c86aa63681fa9057b3b
Andrew Puglionesi <aop4@pitt.edu>
2019-08-18 20:36:57 -0400

    Use string constant

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,10 +2,12 @@ require 'flickraw'
 
 class PhotosController < ApplicationController
 
+  PHOTOSET_ID = '72157710380746362'
+
   def index
     FlickRaw.api_key = ENV['FLICKR_API_KEY']
     FlickRaw.shared_secret = ENV['FLICKR_API_SECRET']
-    photoset = flickr.photosets.getPhotos(:photoset_id => '72157710380746362')
+    photoset = flickr.photosets.getPhotos(:photoset_id => PHOTOSET_ID)
     render json: photoset
   end
 end
