6221aa7122a33080c706a2de4c7536e7b780a892
Andrew Puglionesi <aop4@pitt.edu>
2020-06-27 21:55:19 -0400

    Update song PATCH route to track download counts

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,19 @@
 class SongsController < ApplicationController
   def update
     @song = Song.find(params[:id])
-    @song.play_count += 1
+
+    download_count = params[:download_count]
+    play_count = params[:play_count]
+
+    if !download_count.nil? && download_count > 0
+      # increment the download count
+      @song.download_count += 1
+    end
+    if !play_count.nil? && play_count > 0
+      # increment the play count
+      @song.play_count += 1
+    end
+    
     @song.save
     render json: {}, status: :ok
   end
